937507496679236f2d1d3ab90fafe59a23855b51
tpeulen <thomas.otavio.peulen@gmail.com>
2021-04-15 17:19:37 -0700

    Use conda cxx compiler

--- setup.py
+++ setup.py
@@ -124,7 +124,8 @@ class CMakeBuild(build_ext):
                 env=env
             )
             subprocess.check_call(
-                ["python", "doxy2swig.py", "../docs/_build/xml/index.xml", "../pyext/documentation.i"],
+                ["python", "./build_tools/doxy2swig.py", 
+                "../docs/_build/xml/index.xml", "../pyext/documentation.i"],
                 cwd=str(working_directory / "utility"),
                 env=env
             )
