b2b06a9017fe225551363c0706ae7235d447f7d7
tpeulen <thomas.peulen@posteo.de>
2021-05-11 18:18:17 +0000

    Cleanup

--- include/info.h
+++ include/info.h
@@ -2,6 +2,6 @@
 #define CHINET_INFO_H
 
 #define CHINET_VERSION             "0.0.8"
-
+//#define CHINET_VERBOSE true
 
 #endif //CHINET_INFO_H
--- test/constants.py
+++ test/constants.py
@@ -7,8 +7,10 @@ DB_DICT = {
     'app_string': "chisurf",
     'collection_string': "test_collection"
 }
+
 def connects_to_db():
     mo = cn.MongoObject()
     mo.connect_to_db(**DB_DICT)
     return mo.is_connected_to_db 
+
 CONNECTS = connects_to_db()
