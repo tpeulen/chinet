ab7c0c5f9cee322f019fdeedb39c4f062842401c
tpeulen <thomas.peulen@posteo.de>
2021-05-14 00:48:28 +0000

    Build SWIG doc by cmake

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,11 +1,4 @@
 git submodule update --recursive --init --remote
 rmdir build /s /q
-cd doc
-doxygen
-%PYTHON% ../tools/doxy2swig.py ./_build/xml/index.xml ../ext/python/documentation.i
-cd ..
-%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
-REM remove potentially existing build dir to avoid conflicts with previous builds
-rmdir build /s /q
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 git submodule update --recursive --init --remote
 rm -r -f build
-cd doc
-doxygen
-$PYTHON ../tools/doxy2swig.py _build/xml/index.xml ../pyext/documentation.i
-cd ..
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
