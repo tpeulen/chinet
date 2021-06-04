574a4e33b2388838d0b35daef483fdae00da43f0
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2021-06-04 19:08:03 +0000

    Update conda-recipe/bld.bat

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -2,6 +2,6 @@ git submodule update --recursive --init --remote
 rmdir build /s /q
 cd doc
 doxygen
-%PYTHON% ../tools/doxy2swig.py ./_build/xml/index.xml ../ext/python/documentation.i
+%PYTHON% ../tools/doxy2swig.py ./_build/xml/index.xml ../pyext/documentation.i
 cd ..
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
