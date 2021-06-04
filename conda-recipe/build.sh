7cd457ae4cd192e9f91e79c6b18ebc27a922386e
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2021-06-04 20:33:22 +0000

    Update conda-recipe/build.sh

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -3,6 +3,6 @@ git submodule update --recursive --init --remote
 rm -r -f build
 cd doc
 doxygen
-$PYTHON ../tools/doxy2swig.py _build/xml/index.xml ../pyext/documentation.i
+python ../tools/doxy2swig.py _build/xml/index.xml ../pyext/documentation.i
 cd ..
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+python setup.py install --single-version-externally-managed --record=record.txt
