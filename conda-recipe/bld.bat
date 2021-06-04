afcaa2b0d506f887b6210631f6816c7f98104bae
Thomas-Otavio Peulen <thomas.peulen@posteo.de>
2021-06-04 18:34:39 +0000

    Update conda-recipe/bld.bat, conda-recipe/meta.yaml files

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -5,7 +5,3 @@ doxygen
 %PYTHON% ../tools/doxy2swig.py ./_build/xml/index.xml ../ext/python/documentation.i
 cd ..
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
-REM remove potentially existing build dir to avoid conflicts with previous builds
-rmdir build /s /q
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
-
