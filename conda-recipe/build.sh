aa51fdf6b45befac2397b713f0b2bf9fee018579
tpeulen <thomas.peulen@posteo.de>
2023-03-13 14:15:49 +0100

    Build issues

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git submodule update --recursive --init --remote
-rm -r -f build
+rm -rf build
 cd doc
 doxygen
 $PYTHON ../tools/doxy2swig.py _build/xml/index.xml ../pyext/documentation.i
