9f53c89e3b679ae974944e0c7d24640dd307fb2e
Thomas-Otavio Peulen <tpeulen@peulen.xyz>
2023-03-14 07:39:58 +0000

    Update conda-recipe/build.sh, setup.py

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 git submodule update --recursive --init --remote
 rm -rf build
 cd doc
 doxygen
 $PYTHON ../tools/doxy2swig.py _build/xml/index.xml ../pyext/documentation.i
 cd ..
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+python setup.py install --single-version-externally-managed --record=record.txt
