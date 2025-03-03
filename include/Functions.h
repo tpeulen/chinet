04363dcc93c9795eae7eb7b78caf6b3d7509d113
tpeulen <thomas.peulen@posteo.de>
2025-03-03 23:19:24 +0100

    Fix issues: * clamp not compiling on win

--- include/Functions.h
+++ include/Functions.h
@@ -145,7 +145,7 @@ namespace Functions {
     {
         for(int i = 0; i<n_values; i++)
         {
-            values[i] = std::clamp(values[i], static_cast<T>(lower_bound), static_cast<T>(upper_bound));
+          values[i] = (T) std::min(std::max((double)values[i], lower_bound), upper_bound);
         }
     }
 
